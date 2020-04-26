1f01b2ee34e76496c3e8fec8b8ee7d0fbfa0bcb9
DuckDuckKrya <39408929+DuckDuckKrya@users.noreply.github.com>
2020-04-26 21:37:00 +1000

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,36 @@
+import time
+from random import randint
+print("████████╗███████╗░██████╗████████╗  ███╗░░██╗░█████╗░")
+print("╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝  ████╗░██║██╔══██╗")
+print("░░░██║░░░█████╗░░╚█████╗░░░░██║░░░  ██╔██╗██║███████║")
+print("░░░██║░░░██╔══╝░░░╚═══██╗░░░██║░░░  ██║╚████║██╔══██║")
+print("░░░██║░░░███████╗██████╔╝░░░██║░░░  ██║░╚███║██║░░██║")
+print("░░░╚═╝░░░╚══════╝╚═════╝░░░░╚═╝░░░  ╚═╝░░╚══╝╚═╝░░╚═╝")
+print("██████╗░░█████╗░██╗░░░░░██████╗░░█████╗░███████╗██████╗░░█████╗░")
+print("██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗")
+print("██║░░██║███████║██║░░░░░██████╦╝███████║█████╗░░██████╦╝███████║")
+print("██║░░██║██╔══██║██║░░░░░██╔══██╗██╔══██║██╔══╝░░██╔══██╗██╔══██║")
+print("██████╔╝██║░░██║███████╗██████╦╝██║░░██║███████╗██████╦╝██║░░██║")
+print("╚═════╝░╚═╝░░╚═╝╚══════╝╚═════╝░╚═╝░░╚═╝╚══════╝╚═════╝░╚═╝░░╚═╝")
+time.sleep(5)
+print("segodnya mi uznaem na skolko ti dalbaeb")
+time.sleep(1)
+print("i tak pristupim")
+time.sleep(1)
+print("vischislyau na skolko ti dalbaeb")
+time.sleep(1)
+print("taaaaak echo chut chut")
+print("zavershaem")
+time.sleep(1)
+k = randint(0, 100)
+print("vi dalbaeb na", k, "%")
+if k == 100:
+    print("da ti takoy dalbaeb chto pizdanutsca mozhno")
+else:
+    if k == 0:
+        print("ebat da ti voobche ne dalbaeb")
+    else:
+        if k > 50 and k != 1:
+            print("ebat vi dalbaeb")
+        else:
+            print("pozdravlyaem vi chut chut dalabeb")
